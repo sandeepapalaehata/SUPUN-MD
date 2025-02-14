f6666058eda0b6f3739fa278ca66a6a3794537a0
sandeepapalaehata <sandeepapalayanehata@gmail.com>
2025-02-14 19:50:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=CQVQxKAQ#YkkaU36B6X7xpLTSzxLPvWYNN1nFvOHhJBi3Ce0obFId', //add your session id
 MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
